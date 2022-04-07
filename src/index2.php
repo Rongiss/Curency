47e8e5a8b0ed721356f476cbdfe8c3da4decd9c5
RomanGrigoriev_IPC <rongiss@mail.ru>
2022-04-07 16:36:30 +0800

    add

---
+++ src/index2.php
@@ -0,0 +1,14 @@
+<?php
+$info = file_get_contents('https://www.cbr-xml-daily.ru/daily.xml', true);
+
+$xmlObject = simplexml_load_string($info);
+$out = ['AUD','AZN','KZT','JOPA'];
+$elNotFound=[];
+foreach ($out as $j){
+
+
+}
+
+
+?>
+
