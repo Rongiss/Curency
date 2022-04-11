7727d55842bcfdbea044eadc2c8f1ab83a5a626b
aleksan007 <Aleksan007@mail.ru>
2022-04-11 13:22:46 +0800

    Переименовал переменные в человечячий вид

--- src/index.php
+++ src/index.php
@@ -1,22 +1,28 @@
 <?php
+$data = file_get_contents('https://www.cbr-xml-daily.ru/daily.xml', true);
+$xmlObject = simplexml_load_string($data);
 
-
-$info = file_get_contents('https://www.cbr-xml-daily.ru/daily.xml', true);
-
-$xmlObject = simplexml_load_string($info);
-$out = ['AUD','AZN','KZT','JOPA'];
-foreach ($out as $j){
+$find = ['AUD','AZN','KZT','JOPA'];
+$not_found=[];
+foreach ($find as $find_one){
+    $total = 0;
     foreach ($xmlObject as $lis) {
-        if ($j == $lis->CharCode) {
+
+        if ($find_one == $lis->CharCode) {
+            $total += 1;
             print_r("Valute: " . $lis->Name . ' ');
             print_r("ID: " . $lis->CharCode . " ");
             print_r("Value: " . $lis->Value . "<br>");
         }
     }
+    if ($total == 0){
+        $not_found[] = $find_one;
+    }
+
+}
+$results = array_unique($not_found);
+foreach ($results as $result){
+    print_r("Элемент ".$result." не найден"."<br>");
 }
 
-
-
-
 ?>
-
