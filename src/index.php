93799ff2e9a1c8fc30116e2c9840a745e54ebbc3
RomanGrigoriev_Home <rongiss@mail.ru>
2022-04-18 19:33:20 +0800

    home func input from user

--- src/index.php
+++ src/index.php
@@ -1,8 +1,14 @@
 <?php
+$find_currencies = null;
+if(isset($_GET['currencies'])) {
+    $find_currencies = $_GET['currencies'];
+    $find_currencies = explode(',',$find_currencies);
+}
+print_r($find_currencies);
 
 //Функция преобразования url в обьект
 //возвращает объект для обработки
-function url_to_obj()
+function urlToObject()
 {
     $data = file_get_contents('https://www.cbr-xml-daily.ru/daily.xml', true);
     $xmlObject = simplexml_load_string($data);
@@ -10,26 +16,32 @@ function url_to_obj()
 }
 
 //Функция возвращающая элементы для поиска в api
-function elem_for_search()
+function elemForSearch()
 {
-   return $find = ['AUD', 'AZN', 'KZT', 'JOPAaa', 'Rom'];
+    $find = ["AUD","AZN","GBP","AMD","BYN","BGN","BRL","HUF","HKD",
+       "DKK","USD","EUR","INR","KZT","CAD","KGS","CNY","MDL",
+       "NOK","PLN","RON","XDR","SGD","TJS","TRY","TMT","UZS",
+       "UAH","CZK","SEK","CHF","ZAR","KRW","JPY"];
+    #return $find = ['AUD', 'AZN', 'KZT', 'JOPAaa', 'Rom'];
+
+    return $find;
 }
 
 //функция поиска элементов
 //вывод найденных элементов
 //возвращает не найденные
-function serch()
+function serchElement()
 {
 //пустой массив для заполнения не найденными элементами
     $not_found = [];
-
-    foreach (elem_for_search() as $find_one) {
+    foreach (elemForSearch() as $find_one) {
         $total = 0;
-        foreach (url_to_obj() as $lis) {
+        foreach (urlToObject() as $lis) {
             if ($find_one == $lis->CharCode) {
                 $total += 1;
-                print_r("Valute: " . $lis->Name . ' ');
-                print_r("ID: " . $lis->CharCode . " ");
+                print_r( $lis->CharCode . " "."|"." ");
+                print_r( $lis->Name . " "."|"." ");
+
                 print_r("Value: " . $lis->Value . "<br>");
             }
         }
@@ -42,7 +54,24 @@ function serch()
 }
 
 
-foreach (serch() as $result){
+foreach (serchElement() as $result){
     print_r("Элемент ".$result." не найден"."<br>");
 }
+
+/*function allNamesManoy()
+{
+    foreach (urlToObject() as $lis) {
+        print_r('"'.$lis->CharCode.'"'.',' );
+        #print_r("Value: " . $lis->Value . "<br>");
+    }
+}
+allNamesManoy()*/
+
+
+
 ?>
+<form>
+    <input type="text" name="currencies">
+    <button type="submit">ok</button>
+</form>
+
